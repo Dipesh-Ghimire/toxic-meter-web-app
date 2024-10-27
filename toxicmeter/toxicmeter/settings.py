3c697ed6858a80f41092739fc89a2589b6e0a1c8
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2024-10-27 17:08:25 +0545

    Setup rendering html files

--- toxicmeter/toxicmeter/settings.py
+++ toxicmeter/toxicmeter/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = "toxicmeter.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "DIRS": [BASE_DIR/"templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
