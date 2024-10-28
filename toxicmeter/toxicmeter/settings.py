713611e40fc1a2a5adbf0a610cbd0f1ead27f7ee
Dipesh Ghimire <dipeshghimire.dg@gmail.com>
2024-10-28 22:31:13 +0545

    Setup Cloud-based Postgre SQL Database

--- toxicmeter/toxicmeter/settings.py
+++ toxicmeter/toxicmeter/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+from urllib.parse import urlparse
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,10 +85,19 @@ WSGI_APPLICATION = "toxicmeter.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': {
+            'sslmode': 'require',
+        }
     }
 }
 
